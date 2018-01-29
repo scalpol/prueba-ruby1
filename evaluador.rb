9570252e512b99622e1ef5095a15ffebfb658ea0
scalpol <dazadlp@protonmail.ch>
2018-01-29 11:46:10 -0300

    version final

--- evaluador.rb
+++ evaluador.rb
@@ -10,11 +10,11 @@ def array_to_hash(array)
   array.each do |alumno|
     nombre = ''
     notas = []
-    alumno.each do |i|
-      if i.length > 1 && i != '10'
-        nombre = i.to_sym
+    alumno.length.times do |i|
+      if i.zero?
+        nombre = alumno[i].to_sym
       else
-        notas.push(i.to_i)
+        notas.push(alumno[i].to_i)
       end
       alumnos[nombre] = notas
     end
@@ -33,34 +33,37 @@ end
 asistencia = read_alum('asistencia.csv')
 alumnos = array_to_hash(asistencia)
 seleccion = 0
+opciones = ['(1) Obtener promedios', '(2) Obtener inasistencias', '(3) Obtener aprobados', '(4) Salir']
 
 until seleccion == 4
-  puts '¿Que desea hacer? (1) Obtener promedios (2) Obtener inasistencias (3) Obtener aprobados (4) Salir'
+  puts '¿Que desea hacer?'
+  opciones.each { |opcion| puts opcion }
   seleccion = gets.chomp.to_i
-  if seleccion > 4 || seleccion < 1
-    puts 'Opción inválida.'
-  else
-    case seleccion
+  case seleccion
 
-    when 1
-      alumnos.each do |nombre, notas|
-        promedio = obtener_promedio(notas)
-        puts "#{nombre} tiene promedio #{promedio}."
-      end
-
-    when 2
-      alumnos.each do |nombre, notas|
-        inasistencias = 0
-        notas.each { |nota| inasistencias += 1 if nota == 0 }
-        puts "#{nombre.to_s} tiene  #{inasistencias} inasistencia(s) en total."
-      end
-
-    when 3
-      puts 'Los alumnos aprobados son:'
-      alumnos.each do |nombre, notas|
-        puts nombre if aprobado(notas)
-      end
-
+  when 1
+    alumnos.each do |nombre, notas|
+      promedio = obtener_promedio(notas)
+      puts "#{nombre} tiene promedio #{promedio}."
     end
+
+  when 2
+    alumnos.each do |nombre, notas|
+      inasistencias = 0
+      notas.each { |nota| inasistencias += 1 if nota.zero? }
+      puts "#{nombre} tiene  #{inasistencias} inasistencia(s) en total."
+    end
+
+  when 3
+    puts 'Los alumnos aprobados son:'
+    alumnos.each do |nombre, notas|
+      puts nombre if aprobado(notas)
+    end
+
+  when 4
+    puts 'Adios!'
+
+  else
+    puts 'Opción inválida.'
   end
 end
